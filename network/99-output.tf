f3f2f39b7d52ff0e3c8518b7bfd75d4ebdda2f55
Francesco Boffa <fra.boffa@gmail.com>
2018-04-08 18:29:39 +0100

    Added admin key name to outputs and refactored subnets

--- network/99-output.tf
+++ network/99-output.tf
@@ -10,26 +10,14 @@ output "vpn_ip" {
   value = "${aws_eip.openvpn.public_ip}"
 }
 
-output "subnet_public0_id" {
-  value = "${aws_subnet.public.*.id[0]}"
+output "public_subnet_ids" {
+  value = "${aws_subnet.public.*.id}"
 }
 
-output "subnet_public1_id" {
-  value = "${aws_subnet.public.*.id[1]}"
+output "private_subnet_ids" {
+  value = "${aws_subnet.private.*.id}"
 }
 
-output "subnet_public2_id" {
-  value = "${aws_subnet.public.*.id[2]}"
-}
-
-output "subnet_private0_id" {
-  value = "${aws_subnet.private.*.id[0]}"
-}
-
-output "subnet_private1_id" {
-  value = "${aws_subnet.private.*.id[1]}"
-}
-
-output "subnet_private2_id" {
-  value = "${aws_subnet.private.*.id[2]}"
+output "admin_key_name" {
+  value = "${aws_key_pair.admin.key_name}"
 }
